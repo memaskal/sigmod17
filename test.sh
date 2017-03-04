e15c53dd13a3589c6eb0a8b50d01ec7f1583e51a
memaskal <kalourakos@hotmail.com>
2017-03-04 07:15:15 -0500

    Changed harness executable to .out, so it would be excluded by the .gitignore

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@ cd datasets
 
 # Run our executable with small input
 if [ -z ${1+x} ]; then # No shell input. Normal execute
-	./harness small.init small.work small.result ../source.out
+	./harness.out small.init small.work small.result ../source.out
 else # Forward $1 to harness
-	./harness small.init small.work small.result ../source.out "$1"
+	./harness.out small.init small.work small.result ../source.out "$1"
 fi
 
