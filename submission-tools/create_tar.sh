7b93098c62a3754d676f4947686bb23c97c5291b
harrycoderk <fox.dealos@gmail.com>
2017-03-12 15:13:14 +0200

    Fix submission replacing files

--- submission-tools/create_tar.sh
+++ submission-tools/create_tar.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rm ./Makefile
 cp ../Makefile ./Makefile
 mkdir ./src/
+rm ./src/source.cpp
 cp ../src/source.cpp ./src/source.cpp
 tar -cvf ~/submission.tar.gz README Makefile run.sh compile.sh ./src/source.cpp
